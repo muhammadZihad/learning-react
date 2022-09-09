cc596575d9484355c03305cf3092d04a4987c76e
muhammadZihad <zihad.muhammadar@gmail.com>
2022-09-09 15:20:25 +0600

    Uploading basic project

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+    return (
+        <div className='expense-item'>
+            <div>March 28th, 2022</div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className='expense-item__price'>$290.90</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import '../assets/css/styles.css';
+
+function home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h2>Working With React</h2>
+            </header>
+        </div>
+    );
+}
+
+export default home;
